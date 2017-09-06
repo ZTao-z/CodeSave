e6ccb3976ed2609de365c2f695e137899e6808f1
ZhuTao Huang <1124693098@qq.com>
2017-09-06 17:58:18 +0800

    change insertionsort

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,4 +1,8 @@
-void insertionSort(int* A, int size){
+#include <iostream>
+
+using namespace std;
+
+void insertionSortRaise(int* A, int size){
 	int key, i;
 	for(int j = 1; j < size; j++){
 		key = A[j];
@@ -12,3 +16,24 @@ void insertionSort(int* A, int size){
 	}
 }
 
+void insertionSortDown(int* A, int size){
+	int key, i;
+	for(int j = size-2; j >= 0; j--){
+		key = A[j];
+		//insert A[j] into the sorted sequence
+		i = j + 1;
+		while(i < size && A[i] > key){
+			A[i-1] = A[i];
+			++i;
+		}
+		A[i-1] = key;
+	}
+}
+
+int main(){
+	int k[5] = {1,2,3,4,5};
+	insertionSortDown(k,5);
+	int a = 0;
+	while(a < 5)
+		cout << k[a++];
+}
