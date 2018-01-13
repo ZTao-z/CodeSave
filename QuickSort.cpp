906d81a63b0ed59c74c028df7f32347addfcfd28
huangzhutao <1124693098@qq.com>
2018-01-13 11:49:48 +0800

    change QuickSort content
    refresh

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,29 +1,27 @@
-#define SWAP(a,b){ int t; t = a; a = b; b = t; }
-
-
-int partition(E number[], int left, int right)
-{
-    int i, j, s;
-
-    s = number[right];
-    i = left-1;
-
-    for(j = left; j < right; j++){
-        if(number[j] <= s){
-            i++;
-            SWAP(number[i],number[j]);
-        }
-    }
-    SWAP(number[i+1], number[right]);
-    return i+1;
+void swap(int& a, int& b){
+	int t = a;
+	a = b;
+	b = t;
 }
 
-void quicksort(E number[], int left, int right)
-{
-    int q;
-    if(left < right){
-        q = partition(number,left,right);
-        quicksort(number,left,q-1);
-        quicksort(number,q+1,right);
-    }
+int partition(int* arr, int left, int right){
+	int privot = arr[left];
+
+	while(left < right){
+		while(left < right && arr[right] > privot) right--;
+		swap(arr[left], arr[right]);
+		while(left < right && arr[left] < privot) left++;
+		swap(arr[left], arr[right]);
+	}
+
+	arr[left] = privot;
+	return left;
+}
+
+void quicksort(int* arr, int left, int right){
+	if(left < right){
+		int q = partition(arr, left, right);
+		quicksort(arr, left, q-1);
+		quicksort(arr, q+1, right);
+	}	
 }
